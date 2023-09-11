7cb69f604a8506e82989d9e0f70f6e2cf5f43ee3
jusung.kim <wntjd200@naver.com>
2023-09-11 14:37:28 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,30 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
-fun main(){
-    runBlocking {
+fun main() = runBlocking{
+    doOneTwoThree()
+    println("runBlocking: ${Thread.currentThread().name}")
+    println("5!")
+}
 
+suspend fun doOneTwoThree() = coroutineScope {
+    val job = launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1000L)
+        println("3!")
+    }
+    job.join()
 
-
-
+    launch {
+        println("launch2: ${Thread.currentThread().name}")
+        println("1!")
     }
 
+    repeat(1000) {
+        launch {
+            println("launch3: ${Thread.currentThread().name}")
+            delay(500L)
+            println("2!")
+        }
+    }
+    println("4!")
 }
